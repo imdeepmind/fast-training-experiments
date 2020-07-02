10912bf6543606c38366f69f6fdcfcb5ae27e5df
Abhishek Chatterjee (imdeepmind) <abhishekchatterjeejit@gmail.com>
2020-07-02 22:29:19 +0530

    minor updates to the cpde

--- model.py
+++ model.py
@@ -21,7 +21,8 @@ model.add(Dense(n_nodes=10))
 
 model.build()
 
-model.compile(optimizer=Adam(), loss_function=CrossEntropyLoss(), metrics=["accuracy"])
+model.compile(optimizer=Adam(learning_rate=0.001, betas=(0.9, 0.999), eps=1e-08,
+                             weight_decay=0.0, amsgrad=False), loss_function=CrossEntropyLoss(), metrics=["accuracy"])
 
 print(model.summary())
 
@@ -53,7 +54,8 @@ y_validation = y[int(n*0.8):]
 
 del X, y
 
-model.fit(train_data=(X_train, y_train), validation_data=(X_validation, y_validation), epochs=5, batch_size=32)
+model.fit(train_data=(X_train, y_train), validation_data=(
+    X_validation, y_validation), epochs=5, batch_size=32)
 
 ev = model.evaluate(X=X_test, y=y_test, batch_size=32)
 
